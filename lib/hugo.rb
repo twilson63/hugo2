4320bc7d26cc75b89f883515ee8b848e10dd518b
Tom Wilson <tom@jackhq.com>
2009-12-05 22:17:36 -0500

    added interface template for hugo2

--- lib/hugo.rb
+++ lib/hugo.rb
@@ -9,3 +9,53 @@ require 'net/ssh'
 require 'hugo/rds'
 require 'hugo/elb'
 require 'hugo/ec2'
+
+module Hugo
+  
+  class << self
+    def build(infrastructure, application, instances = 1)
+      app_config = YAML.load_file("config/#{application}.yml")
+      @rds = Rds.new(:server => infrastructure, :db => application, :user => app_config["db"]["user"], :pwd => app_config["db"]["password"])
+      @rds.save
+      @elb = Elb.new(:name => infrastructure)
+      @elb.save
+      
+      instances.times do 
+        @ec2 = Ec2.new()
+        @ec2.save
+        @elb.add(@ec2.name)
+      end
+      
+      setup(infrastructure, application)
+      
+      deploy(infrastructure, application)
+      
+    end
+    
+    def drop(infrastructure)
+      Rds.find(infrastructure).destroy
+      @elb = Elb.find(infrastructure)
+      @elb.instances.each do |i|
+        Ec2.find(i).destroy
+      end
+      @elb.destroy
+    end
+    
+    def setup(infrastructure, application)
+      @elb = Elb.find(infrastructure)
+      @elb.instances.each do |i|
+        Ec2.find(i).ssh()
+      end
+      
+    end
+    
+    def deploy(infrastructure, application)
+      @elb = Elb.find(infrastructure)
+      @elb.instances.each do |i|
+        Ec2.find(i).ssh()
+      end
+    end
+    
+  end
+  
+end
