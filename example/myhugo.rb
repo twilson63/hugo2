cdc93bcf09c8a614d58d954deab66e057c3df6fa
Barrett & Andrew <thing1@jackhq.com>
2009-12-29 12:32:05 -0500

    Updated example DSL.

--- example/myhugo.rb
+++ example/myhugo.rb
@@ -1,27 +1,25 @@
-Hugo do
-  
-  cloud "revcare" do
-    
-    database do |db|
-      db.user ""
-      db.pass ""
-    end
-    
-    balancer do |lb|
-      port 8080
-      ssl_port 8443
-    end
-    
-    2.times do
-      instance "eirene4" do
-        role :rails
-        deploy true
-        migrate true
-      end
-    end
-  end
-end
+require '../lib/hugo'
 
-    
-      
-    
+Hugo do
+ cloud "client" do
+   @instances = 2
+   @application = "example"
+
+   database do
+     @user = "dbsa"
+     @pass = "dbpass"
+   end
+
+   balancer
+
+   @package_list = [{"name"=>"mysql-client"}]
+   @gem_list = [{"name"=>"rack"}, {"name"=>"rails", "version"=>"2.3.5"}]
+   @run_list = ["role[web-app]"]
+
+   @github_url = "git@github.com:example"
+   @privatekey = "-----BEGIN RSA PRIVATE KEY-----\nxxxxxx-------\n-----END RSA PRIVATE KEY-----\n"
+   @publickey = "ssh-rsa xxxxxxxxx== progger@example.com\n"
+
+   @app_info = { :branch => "master", :migrate => true }
+ end
+end
