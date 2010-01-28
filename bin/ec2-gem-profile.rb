28662fc5e74efb53b4fc0d8549c8f76ffdb55f63
Tom Wilson <tom@jackhq.com>
2010-01-27 19:59:35 -0500

    added gem dependencies2

--- bin/ec2-gem-profile.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/ruby
-require File.join(File.dirname(__FILE__), "../vendor/gems/environment")
-load File.join(File.dirname(__FILE__), "../vendor/gems/gems/amazon-ec2-0.7.9/bin/ec2-gem-profile.rb")
